318f9505299df419443cd1c9ea61b27dc101e7be
582324909 <582324909@qq.com>
2016-12-08 16:02:15 +0800

    MBProgressHUD
    The use of the MBProgressHUD method

--- MBProgressHUD/ViewController.m
+++ MBProgressHUD/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD.h"
 
 @interface ViewController ()
+
+@property (atomic, assign) BOOL canceled;
 
 @end
 
@@ -19,6 +22,131 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+//仅显示加载的图标
+- (IBAction)indeterminateExample:(id)sender
+{
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+        [self doSomeWork];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [hud hideAnimated:YES];
+        });
+    });
+}
+
+- (IBAction)With_details_label:(id)sender
+{
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.label.text = NSLocalizedString(@"Loading...", @"HUD loading title");
+    hud.detailsLabel.text = NSLocalizedString(@"Parsing data\n(1/1)", @"HUD title");
+    
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+        [self doSomeWork];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [hud hideAnimated:YES];
+        });
+    });
+}
+
+- (IBAction)With_label:(id)sender
+{
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.label.text = @"正在加载";
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+        [self doSomeWork];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [hud hideAnimated:YES];
+        });
+    });
+}
+
+- (IBAction)Circle_style:(id)sender
+{
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeDeterminate;
+    hud.label.text =  @"正在加载";
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+        [self doSomeWorkWithProgress];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [hud hideAnimated:YES];
+        });
+    });
+}
+
+- (IBAction)Progress_style:(id)sender
+{
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeDeterminateHorizontalBar;
+    hud.label.text = @"正在加载";
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+        [self doSomeWorkWithProgress];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [hud hideAnimated:YES];
+        });
+    });
+}
+
+- (IBAction)Text_only:(id)sender
+{
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeText;
+    hud.label.text = @"正在加载";
+    hud.offset = CGPointMake(0.f, MBProgressMaxOffset);
+    
+    [hud hideAnimated:YES afterDelay:3.f];
+}
+
+- (IBAction)With_action_button:(id)sender
+{
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeDeterminate;
+    hud.label.text = @"正在加载";
+    [hud.button setTitle:@"取消" forState:UIControlStateNormal];
+    [hud.button addTarget:self action:@selector(cancelWork:) forControlEvents:UIControlEventTouchUpInside];
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+        [self doSomeWorkWithProgress];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [hud hideAnimated:YES];
+        });
+    });
+}
+
+- (IBAction)Custom_view:(id)sender
+{
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeCustomView;
+    UIImage *image = [[UIImage imageNamed:@"Checkmark"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    hud.customView = [[UIImageView alloc] initWithImage:image];
+    hud.square = YES;
+    hud.label.text = @"已完成";
+    [hud hideAnimated:YES afterDelay:1.5f];
+}
+
+- (void)doSomeWorkWithProgress {
+    self.canceled = NO;
+    float progress = 0.0f;
+    while (progress < 1.0f) {
+        if (self.canceled) break;
+        progress += 0.01f;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [MBProgressHUD HUDForView:self.view].progress = progress;
+        });
+        usleep(50000);
+    }
+}
+
+-(void)doSomeWork
+{
+    //加载时候处理的事件和方法
+    NSLog(@"正在加载");
+    
+    //显示加载的时间
+    sleep(1.5);
+}
+
+- (void)cancelWork:(id)sender {
+    self.canceled = YES;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
